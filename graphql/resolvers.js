132e77a8d89f68759fd741d456bebaa757e8dc6f
Jeong Hwan Seo <restnfeel@gmail.com>
2018-11-23 21:01:51 +0900

    graphql first

---
+++ graphql/resolvers.js
@@ -0,0 +1,11 @@
+import { getMovies, getMovie, getSuggestions } from '../db/db';
+
+const resolvers = {
+    Query : {
+        movies : (_, {limit}) => getMovies(limit),
+        movie : (_, {id}) => getMovie(id),
+        suggestions : (_,{id}) => getSuggestions(id)
+    }
+}
+
+export default resolvers;
